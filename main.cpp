60714b396b5656aabe66acfa8a7e55cb071215f6
Hakim <haakimfago@mail.ru>
2023-03-16 01:11:05 +0700

    Case: File not Exist

--- main.cpp
+++ main.cpp
@@ -3,18 +3,24 @@
 #include <file_observer.h>
 #include <subject.h>
 #include <string>
+#include <QFile>
+#include <QTextStream>
 #include <QDebug>
 using namespace std;
 int main()
 {
-    const char* str="t.pdf";
+
+
+
+
+    const char* str="Observe_example.txt";        //#1 пример файл не существует
     Monitor monitor( str );                       //создаем объект для отслеживания состояния конкретного файла
     File_observer obs;                            //определяется объект наблюдатель
     monitor.registerObserver( &obs);              //связываем Наблюдателя с Источником
     while( true )
     {
-        monitor.checkFile();
-        std::this_thread::sleep_for( std::chrono::milliseconds( 2000 ) );
+        monitor.checkFile();                                               //Проверяем на измение файла наблюдателя
+        std::this_thread::sleep_for( std::chrono::milliseconds( 2000 ) );  //Обновлять состояние объекта Источника каждые 2000 миллисекунд
     }
 
     return 0;
